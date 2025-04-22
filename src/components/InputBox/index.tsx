26072c71fe2ebbc0146a13d3b3ca8cefeb59954a
woojin-kim-baam <woogin2885@gmail.com>
2025-04-22 17:16:27 +0900

    마이페이지 수정페이지

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,46 +1,66 @@
-import React, { ChangeEvent } from 'react';
-import './style.css';
+import React, { ChangeEvent } from "react";
+import "./style.css";
 
 // interface: 공통 인풋 박스 컴포넌트 속성 //
 interface Props {
   label: string;
   value: string;
   placeholder: string;
-  type: 'text' | 'password';
+  type: "text" | "password";
   buttonName?: string;
   message: string;
   isErrorMessage?: boolean;
   isButtonActive?: boolean;
   readOnly?: boolean;
+  disable?: boolean;
 
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   onButtonClick?: () => void;
 }
 
 // component: 공통 인풋 박스 컴포넌트 //
 export default function InputBox(props: Props) {
-
-  const { label, value, placeholder, type, buttonName, message, isErrorMessage, isButtonActive, readOnly } = props;
+  const {
+    label,
+    value,
+    placeholder,
+    type,
+    buttonName,
+    message,
+    isErrorMessage,
+    isButtonActive,
+    readOnly,
+    disable
+  } = props;
   const { onChange, onButtonClick } = props;
 
   // variable: 메세지 클래스 //
-  const messageClass = `message ${isErrorMessage ? 'error' : 'success'}`;
+  const messageClass = `message ${isErrorMessage ? "error" : "success"}`;
   // variable: 버튼 클래스 //
-  const buttonClass = `button ${isButtonActive ? 'second' : 'disable'}`
+  const buttonClass = `button ${isButtonActive ? "second" : "disable"}`;
 
   // render: 공통 인풋 박스 컴포넌트 //
   return (
-    <div className='input-box'>
-      <div className='label'>{label}</div>
-      <div className='input-contents'>
-        <div className='input-area'>
-          <input type={type} value={value} placeholder={placeholder} onChange={onChange} readOnly={readOnly} />
-          {onButtonClick && buttonName && 
-          <div className={buttonClass} onClick={onButtonClick}>{buttonName}</div>
-          }
+    <div className="input-box">
+      <div className="label">{label}</div>
+      <div className="input-contents">
+        <div className="input-area">
+          <input
+            type={type}
+            value={value}
+            placeholder={placeholder}
+            onChange={onChange}
+            readOnly={readOnly}
+            disabled={disable}
+          />
+          {onButtonClick && buttonName && (
+            <div className={buttonClass} onClick={onButtonClick}>
+              {buttonName}
+            </div>
+          )}
         </div>
         <div className={messageClass}>{message}</div>
       </div>
     </div>
-  )
-} 
+  );
+}
--- src/views/UserPage/UserPageUpdate/index.tsx
+++ src/views/UserPage/UserPageUpdate/index.tsx
@@ -1,6 +1,28 @@
+import { ChangeEvent, useState } from "react";
+import InputBox from "../../../components/InputBox";
+import useSignInUserStore from "../../../stores/sign-in-user.store";
 import "./style.css";
 
 export default function UserPageUpdate() {
+  // state: 로그인 사용자 정보 //
+  // const {profileImage, introduce, interests, userNickname, phone-Number} = useSignInUserStore();
+
+  // state: 수정 사용자 자기소개 상태 //
+  const [updateIntroduce, setUpdateIntroduce] = useState<string>("");
+  // state: 수정 사용자 닉네임 상태 //
+  const [updateNickName, setUpdateNickName] = useState<string>("");
+
+  // event handler: 사용자 자기소개 변경 이벤트 처리 //
+  const onIntroduceChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setUpdateIntroduce(value);
+  };
+  // event handler: 사용자 닉네임 변경 이벤트 처리 //
+  const onNicknameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setUpdateNickName(value);
+  };
+
   return (
     <div id="update-userpage">
       <div className="profile-container">
@@ -11,29 +33,56 @@ export default function UserPageUpdate() {
 
         <div className="profile-content-container">
           <div className="introduce-container">
-            <div className="introduce">자기소개</div>
             <div className="introduce-content">
-              <input type="text" />
+              <InputBox
+                label="자기소개"
+                value={updateIntroduce}
+                placeholder="자기소개를 작성해주세요"
+                type="text"
+                message=""
+                onChange={onIntroduceChangeHandler}
+              />
             </div>
           </div>
           <div className="interest-container">
             <div className="interest">관심사</div>
             <div className="interest-content">운동</div>
           </div>
+
           <div className="nickname-container">
-            <div className="nickname">닉네임</div>
-            <div className="nickname-content">우진우진</div>
+            <InputBox
+              label="닉네임"
+              value={updateNickName}
+              placeholder="닉네임을 작성해주세요"
+              type="text"
+              message=""
+              onChange={onNicknameChangeHandler}
+            />
+            <div className="nickname-check">중복 확인</div>
           </div>
+
           <div className="phone-number-container">
-            <div className="phone-number">전화번호</div>
-            <div className="phone-number-content">010-1111-2222</div>
+            <InputBox
+              label="전화번호"
+              value="010-1111-1111"
+              placeholder="Naver로 회원가입 하신분은 변경이 안됩니다."
+              type="text"
+              message=""
+              disable
+            />
           </div>
           <div className="password-container">
-            <div className="password">비밀번호</div>
-            <div className="password-content">***********</div>
+            <InputBox
+              label="비밀번호"
+              value="**********"
+              placeholder=""
+              type="text"
+              message=""
+              disable
+            />
           </div>
+          <div className="user-update-save-button">저장</div>
         </div>
-        <div>저장</div>
       </div>
     </div>
   );
