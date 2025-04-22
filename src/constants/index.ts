2158c8cc1458774a7719688e08072dedc57d1861
yuki-k1313 <yuki1313@naver.com>
2025-04-22 17:40:27 +0900

    게시판 관련 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -45,8 +45,8 @@ export const BOARD_VIEW_ABSOLUTE_PATH = (boardSequence: number | string) =>
 export const BOARD_UPDATE_ABSOLUTE_PATH = (boardSequence: number | string) =>
     `${ROOT_PATH}${BOARD_PATH}/${boardSequence}/${BOARD_UPDATE_PATH}`;
 
-export const GET_BOARD_LIST_URL = (tag: string, page: number, sort: string = 'LATEST') =>
-    `${BOARD_ABSOLUTE_PATH}/${tag}/${page}?sortOption=${sort}`;
+export const GET_BOARD_LIST_ABSOLUTE_PATH = (tag: string) =>
+  `${BOARD_ABSOLUTE_PATH}/${tag}`;
 
 export const DAILY_ABSOLUTE_PATH = `${ROOT_PATH}${DAILY_PATH}`;
 
